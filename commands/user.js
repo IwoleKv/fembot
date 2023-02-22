6bcc63b468e7fabcb8524fe95bef3ac122574ab5
IwoleKv <IwoleKv@gmail.com>
2023-02-22 04:06:47 +0100

    1

--- commands/user.js
+++ commands/user.js
@@ -1,10 +1,24 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
 		.setDescription('Provides information about the user.'),
 	async execute(interaction) {
-		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+
+const exampleEmbed = new EmbedBuilder()
+	.setColor(0xff006f)
+	.setTitle('Informacje:')
+	.setAuthor({ name: `${interaction.user.tag}`, iconURL: `${interaction.member.displayAvatarURL()}` })
+	.addFields(
+		{ name: 'Pseudonim', value: `\`${interaction.user.username}\``, inline: true },
+		{ name: 'ID', value: `\`${interaction.member.id}\``, inline: true },
+		{ name: 'Avatar', value: ' ', inline: false },
+	)
+	.setImage(`${interaction.member.displayAvatarURL()}?size=512`)
+	.setTimestamp()
+	.setFooter({ text: 'WIP - IwoleK', iconURL: 'https://cdn-icons-png.flaticon.com/512/8111/8111673.png' });
+		
+		await interaction.reply({embeds: [exampleEmbed]});
 	},
 };
