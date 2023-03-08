138b8494be276c75fa399bc662ab72073725ecc4
IwoleKv <IwoleKv@gmail.com>
2023-03-08 20:27:01 +0100

    .

--- commands/echo.js
+++ commands/echo.js
@@ -3,7 +3,8 @@ const {
 	ChatInputCommandInteraction,
 	AutocompleteInteraction,
 	EmbedBuilder,
-	Events
+	Events,
+	codeBlock,
 } = require('discord.js');
 
 module.exports = {
@@ -15,13 +16,18 @@ module.exports = {
 				.setName('tabelka')
 				.setDescription('kolor')
 				.addStringOption((option) =>
-					option.setName('kolor').setDescription('kolor').setRequired(true)
-						.setAutocomplete(true))
-				.addStringOption((option) =>
 					option
 						.setName('tekst')
 						.setDescription('Tekst wiadomości')
+						.setMaxLength(4096)
 						.setRequired(true),
+				)
+				.addStringOption((option) =>
+					option
+						.setName('kolor')
+						.setDescription('czerwony')
+						.setRequired(false)
+						.setAutocomplete(true),
 				)
 				.addChannelOption((option) =>
 					option
@@ -46,50 +52,100 @@ module.exports = {
 						.setDescription('Wybrany kanał')
 						.setRequired(false),
 				),
-		),
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName('codeblock'))
+
 	/**
    *
    * @param {ChatInputCommandInteraction} interaction
    * @param {AutocompleteInteraction} interaction
    */
+
 	async autocomplete(interaction) {
 		const focusedValue = interaction.options.getFocused();
-		const choices = ['czerwony', 'niebieski', 'zielony', 'żółty', 'purple', 'pink'];
+		const choices = [
+			'losowy',
+			'czerwony',
+			'niebieski',
+			'czarny',
+			'biały',
+			'fioletowy',
+			'różowy',
+			'pomarańczowy',
+			'żółty',
+			'zielony',
+		];
 		const filtered = choices.filter((choice) =>
 			choice.startsWith(focusedValue),
 		);
 		await interaction.respond(
-			filtered.map((choice) => ({ name: choice, value: choice })),
+			filtered.map((choice) => ({ name: choice, value: choice, inline: true })),
 		);
 	},
+
 	async execute(interaction) {
-		// const colors = [
-		// 	{ value: 0xED4245, name: 'czerwony' },
-		// 	{ value: 0x57F287, name: 'zielony' },
-		// 	{ value: 0xFEE75C, name: 'żółty' },
-		// ];
-		// eslint-disable-next-line no-undef
-		const kolor = interaction.options.getString('kolor');
+		let kolor = interaction.options.getString('kolor');
 		const emb = interaction.options.getSubcommand('tabelka');
+		const codeblock = interaction
+		const txt_ = interaction.options.getString('tekst');
 		const channel_ =
       interaction.options.getChannel('kanał') || interaction.channel;
-		const txt_ = interaction.options.getString('tekst');
-		const tab = {
-			color: kolor,
-			title: txt_,
-		};
+
+		switch (kolor) {
+		case 'czerwony':
+			kolor = '0xff3333';
+			break;
+		case 'niebieski':
+			kolor = '0x0095ff';
+			break;
+		case 'czarny':
+			kolor = '0x000000';
+			break;
+		case 'biały':
+			kolor = '0xFFFFFF';
+			break;
+		case 'fioletowy':
+			kolor = '0x9400d9';
+			break;
+		case 'różowy':
+			kolor = '0xff0099';
+			break;
+		case 'pomarańczowy':
+			kolor = '0xff9100';
+			break;
+		case 'żółty':
+			kolor = '0xfaed5a';
+			break;
+		case 'zielony':
+			kolor = '0x00b809';
+			break;
+		case 'losowy':
+			kolor = 'Random';
+			break;
+		default:
+			kolor = 'Random';
+			break;
+		}
+
+		const tab = new EmbedBuilder().setColor(`${kolor}`)
+			// .addFields(
+			// 	{ name: '** **', value: txt_ });
+			.setDescription(txt_);
 		if (emb === 'tabelka') {
 			await interaction.reply({
 				content:
-          'Dummy message bo API jest zjebane :), ignoruj i tak tylko ty to widzisz',
+          '(zjebane API DC - ignoruj, tylko ty to widzisz)',
 				ephemeral: true,
 			});
 			await channel_.send({ embeds: [tab] });
-		}
-		else {
+		}else if(){
+
+		}else {
 			await interaction.reply({
 				content:
-          'Dummy message bo API jest zjebane :), ignoruj i tak tylko ty to widzisz',
+          '(zjebane API DC - ignoruj, tylko ty to widzisz)',
 				ephemeral: true,
 			});
 			await channel_.send(txt_);
